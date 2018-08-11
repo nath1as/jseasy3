efdb905476c3dffaef580c75abfd875bd729bd45
nathias <n@th1.as>
2018-08-11 13:28:51 +0200

    letter counter

---
+++ 9.js
@@ -0,0 +1,23 @@
+function wordSizes(string) {
+  var wordsArray = string.split(' ');
+  var i;
+  var count = {};
+  var wordSize;
+
+  for (i = 0; i < wordsArray.length; i++) {
+    wordSize = wordsArray[i].length;
+    if (wordSize === 0) {
+      continue;
+    }
+
+    count[wordSize] = count[wordSize] || 0;
+    count[wordSize] += 1;
+  }
+  console.log(count);
+}
+
+wordSizes('Four score and seven.');                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
+wordSizes('Hey diddle diddle, the cat and the fiddle!');  // { "3": 5, "6": 1, "7": 2 }
+wordSizes("What's up doc?");                              // { "2": 1, "4": 1, "6": 1 }
+wordSizes('');                                            // {}
+
