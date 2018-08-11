12989a4eafbd72c59614dba32705c4ac3d2147b1
nathias <n@th1.as>
2018-08-11 13:15:00 +0200

    swap

---
+++ 8.js
@@ -0,0 +1,22 @@
+function swap(words) {
+  var wordsArray = words.split(' ');
+  var i;
+
+  for (i = 0; i < wordsArray.length; i += 1) {
+    wordsArray[i] = swapFirstLastCharacters(wordsArray[i]);
+  }
+
+  return wordsArray.join(' ');
+}
+
+function swapFirstLastCharacters(word) {
+  if (word.length === 1) {
+    return word;
+  }
+
+  return word[word.length - 1] + word.slice(1, -1) + word[0];
+}
+swap('Oh what a wonderful day it is');  // "hO thaw a londerfuw yad ti si"
+swap('Abcde');                          // "ebcdA"
+swap('a');                              // "a"
+
